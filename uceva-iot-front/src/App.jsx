08ff06ed07677f21e879f6f298e5407ea9a3e5b2
JaimeDiaz <jedt1996@gmail.com>
2024-01-15 16:10:17 -0500

    Add Layout for all pages

--- uceva-iot-front/src/App.jsx
+++ uceva-iot-front/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "./Routes/AppRoutes";
 import Navbar from "./Components/Navbar";
+import Layout from "./Components/Layout";
 
 function App() {
 
   return (
     <BrowserRouter>
-      <AppRoutes />
       <Navbar />
+      <Layout>
+        <AppRoutes />
+      </Layout>
     </BrowserRouter>
   )
 }
